93737af26f3220de279875a8c9bb9639c49150d8
gustavo <gustavodellac@gmail.com>
2025-04-25 17:59:38 -0300

    se configura dockerfile y docker-compose para configurar imagenes segun entorno, se agrega nodemon json para seguimiento dentro del contenedor

--- dockerfile
+++ dockerfile
@@ -4,9 +4,14 @@ WORKDIR /app
 
 COPY . .
 
-#instalar dependencias de produccion
-RUN npm install --production
+# definimos el environment segun el entorno
+ARG NODE_ENV=production
+ENV NODE_ENV=$NODE_ENV
+
+# instalamos dependencias segun el entorno
+RUN if [ "$NODE_ENV" = "production" ]; then npm install --production; else npm install; fi
 
 EXPOSE 3000
 
+# si no existe command en docker compose se ejecuta este comando al ejecutar el contenedor
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config(); // Cargar .env
 
 const app = express();
 const environment = process.env.NODE_ENV || 'development';
-const port = process.env.PORT_HOST || 3000;
+const port = process.env.PORT || 3000;
 
 console.log(`initializing ${environment} mode`);
 
@@ -13,11 +13,12 @@ app.use(express.json());
 
 // Ruta de prueba GET
 app.get('/', (req, res) => {
-  res.send('¡Hola! La API está funcionando. test');
+  res.send(`ENVIRONMENT ${environment} - API OK`);
 });
 
 // Levantar el servidor escuchando en todas las interfaces para que el host
 //pueda acceder al contenedor
 app.listen(port, '0.0.0.0', () => {
-  console.log(`API listening en http://localhost:${port}`);
+  console.log(`ENVIRONMENT ${environment} - API listening en http://localhost:${port}`);
 });
+
